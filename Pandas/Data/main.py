88429ab7e48187ef0372d6a721b0dcaf412d1495
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-06-11 05:58:08 +0900

    Add files via upload

---
+++ Pandas/Data/main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+data = {'Name':['Alice','Bob','Charlie'],
+        'Age':[25,30,22],
+        'City':['New York','San Francisco','Los Angelos']}
+
+df = pd.DataFrame(data)
+
+print("Original DataFrame:")
+print(df)
+
+# names_column = df['Name']
+# print("\nName column:")
+# print(names_column)
+
+# df['Salary'] = [50000,60000,45000]
+
+# print("\nUpdated DataFrame with Salary column:")
+# print(df)
+
+# mean_age =df['Age'].mean()
+# print("\nMean Age",mean_age)
+
+# import pandas as pd
+
+# df = pd.DataFrame({'points': [25, 12, 15, 14],
+#                    'assists': [5, 7, 13, 12]})
+
+# print(df)
+
+# import pandas as pd 
+  
+# data = {"id": [1, 2, 3], 
+#         "name": ["karthik", "nikhil", "bhagi"]} 
+  
+# df = pd.DataFrame(data) 
+# print(df)
