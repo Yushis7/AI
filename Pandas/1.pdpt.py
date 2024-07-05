5efb2ce590ea34b8f9608e3af756c6b04dacf76b
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-07-06 07:39:49 +0900

    Add files via upload

---
+++ Pandas/1.pdpt.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+#데이터 불러오기
+# df = pd.read_text('C:/Users/sspea/Downloads/CS/만든 거/Ai/pandas/friend_list.txt')
+
+
+# df.head(2) #위에서 2개
+# df.tail(3) #밑에서 3개
+# pd.DataFrame([10,20,30,40,50])
+
+# s1 = pd.Series([10,20,30,40,50])
+# print(s1)
+
+# data = {'Name':['Alice','Bob','Charlie'],
+#         'Age':[25,30,22],
+#         'City':['New York','San Francisco','Los Angelos']}
+
+# df = pd.DataFrame(data)
+
+# print("Original DataFrame:")
+# print(df)
+
